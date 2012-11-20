e558ac6166e90dca4f8fba907379042593856814
Sara Arjona <sarjona@xtec.cat>
2012-11-20 10:43:57 +0100

    Core: Increased default repository timeout from 30 to 150 to let downloading bigger files

--- repository/lib.php
+++ repository/lib.php
@@ -499,7 +499,12 @@ abstract class repository implements cacheable_object {
      * Timeout in seconds for downloading the external file into moodle
      * @deprecated since Moodle 2.7, please use $CFG->repositorygetfiletimeout instead
      */
-    const GETFILE_TIMEOUT = 30;
+    //XTEC ************ MODIFICAT - To increase default timeout (to let downloading bigger files)
+    //2012.11.20 @sarjona
+    const GETFILE_TIMEOUT = 150;
+    //************ ORIGINAL
+    //const GETFILE_TIMEOUT = 30;
+    //************ FI
 
     /**
      * Timeout in seconds for syncronising the external file size
