6d976f83e4bfbaf6190dc018944db39677124be6
Pau Ferrer <upcnet@TAC79.educacio.intranet>
2014-05-14 13:50:22 +0200

    Core: Avoid cron to stop executing when sending mails

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -6155,7 +6155,7 @@ function email_to_user($user, $from, $subject, $messagetext, $messagehtml = '', 
         //add message to mailsender
         if (!$sender->add($message)){
                 mtrace('ERROR: '.' Impossible to add message to mailsender');
-                add_to_log(SITEID, 'library', 'mailer', $FULLME, 'ERROR: '. ' Impossible to add message to mailsender');
+                add_to_log(SITEID, 'library', 'mailer', qualified_me(), 'ERROR: '. ' Impossible to add message to mailsender');
                 return false;
         }
         //send messages
@@ -6215,7 +6215,7 @@ function email_to_user($user, $from, $subject, $messagetext, $messagehtml = '', 
         return false;
     }
     */
-    //************ FI    
+    //************ FI
 }
 
 /**
