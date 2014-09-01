c2e2c36548fd6f878f7159ddb5cac2a89822f68e
pau.ferrer-ocana <pau.ferrer-ocana@upcnet.es>
2014-09-01 12:17:16 +0200

    Agora: Add options log in via Oauth to other platforms like Nodes

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -4113,6 +4113,10 @@ function create_user_record($username, $password, $auth = 'manual') {
     } else {
     	$newuser->username = $username;
     }
+    //************ ORIGINAL
+    /*
+    $newuser->username = $username;
+    */
     //************ FI
 
     // Fix for MDL-8480
