33459e79391627879d7dea7ea1dc1f6d5f04d966
pau.ferrer-ocana <pau.ferrer-ocana@upcnet.es>
2014-11-20 10:09:07 +0100

    Core: Changes made for 2.4.0 version (Split needed)  - Protect uninstall  - Protect Settings  - CAS auth changes  - LDAP auth changes  - Config oracle and mysql  - Control backup hours  - Erased patch on course/lib.php  - And many more Original commit: sarjona authored on 11 Dec 2012

--- report/loglive/settings.php
+++ report/loglive/settings.php
@@ -26,9 +26,17 @@
 
 defined('MOODLE_INTERNAL') || die;
 
+//XTEC ************ AFEGIT - To let access only to xtecadmin user
+//2012.06.20  @sarjona
+if (get_protected_agora() ) {
+//************ FI
 // Just a link to course report.
 $ADMIN->add('reports', new admin_externalpage('reportloglive', get_string('pluginname', 'report_loglive'),
         "$CFG->wwwroot/report/loglive/index.php", 'report/loglive:view'));
+//XTEC ************ AFEGIT - To let access only to xtecadmin user
+//2012.06.20  @sarjona
+}
+//************ FI
 
 // No report settings.
 $settings = null;
