6eeb8c77bbe55200e087bf11df803e7858ba8f31
Davo Smith <git@davosmith.co.uk>
2014-12-01 10:27:21 +0000

    MDL-43462 assignfeedback_editpdf: fix display of landscape PDFs
    The size of the drawing canvas is now set from the size of the generated page image, with automatic
    scrolling if it is too wide to display
    Absolutely-positioned nodes (comments + stamps) are repositioned as the drawing canvas is scrolled
    
    Conflicts:
    	mod/assign/feedback/editpdf/yui/build/moodle-assignfeedback_editpdf-editor/moodle-assignfeedback_editpdf-editor-min.js

--- mod/assign/feedback/editpdf/yui/src/editor/js/editor.js
+++ mod/assign/feedback/editpdf/yui/src/editor/js/editor.js
@@ -282,7 +282,7 @@ EDITOR.prototype = {
      * @method link_handler
      */
     link_handler : function(e) {
-        var drawingcanvas;
+        var drawingcanvas, drawingregion, resize = true;
         e.preventDefault();
 
         if (!this.dialogue) {
@@ -304,6 +304,9 @@ EDITOR.prototype = {
             drawingcanvas = Y.one(SELECTOR.DRAWINGCANVAS);
             this.graphic = new Y.Graphic({render : SELECTOR.DRAWINGCANVAS});
 
+            drawingregion = Y.one(SELECTOR.DRAWINGREGION);
+            drawingregion.on('scroll', this.move_canvas, this);
+
             if (!this.get('readonly')) {
                 drawingcanvas.on('gesturemovestart', this.edit_start, null, this);
                 drawingcanvas.on('gesturemove', this.edit_move, null, this);
@@ -313,10 +316,18 @@ EDITOR.prototype = {
             }
 
             this.load_all_pages();
+            drawingcanvas.on('windowresize', this.resize, this);
+
+            resize = false;
         }
         this.dialogue.centerDialogue();
         this.dialogue.show();
-        drawingcanvas.on('windowresize', this.resize, this);
+
+        // Redraw when the dialogue is moved, to ensure the absolute elements are all positioned correctly.
+        this.dialogue.dd.on('drag:end', this.redraw, this);
+        if (resize) {
+            this.resize(); // When re-opening the dialog call redraw, to make sure the size + layout is correct.
+        }
     },
 
     /**
@@ -829,7 +840,21 @@ EDITOR.prototype = {
      * @method resize
      */
     resize : function() {
+        var drawingregion, drawregionheight;
+
+        if (!this.dialogue.get('visible')) {
+            return;
+        }
         this.dialogue.centerDialogue();
+
+        // Make sure the dialogue box is not bigger than the max height of the viewport.
+        drawregionheight = Y.one('body').get('winHeight') - 120; // Space for toolbar + titlebar.
+        if (drawregionheight < 100) {
+            drawregionheight = 100;
+        }
+        drawingregion = Y.one(SELECTOR.DRAWINGREGION);
+        drawingregion.setStyle('maxHeight', drawregionheight +'px');
+        this.redraw();
         return true;
     },
 
@@ -930,6 +955,9 @@ EDITOR.prototype = {
             page;
 
         page = this.pages[this.currentpage];
+        if (page === undefined) {
+            return; // Can happen if a redraw is triggered by an event, before the page has been selected.
+        }
         while (this.drawables.length > 0) {
             this.drawables.pop().erase();
         }
@@ -970,11 +998,13 @@ EDITOR.prototype = {
         page = this.pages[this.currentpage];
         this.loadingicon.hide();
         drawingcanvas.setStyle('backgroundImage', 'url("' + page.url + '")');
+        drawingcanvas.setStyle('width', page.width + 'px');
+        drawingcanvas.setStyle('height', page.height + 'px');
 
         // Update page select.
         Y.one(SELECTOR.PAGESELECT).set('value', this.currentpage);
 
-        this.redraw();
+        this.resize(); // Internally will call 'redraw', after checking the dialogue size.
     },
 
     /**
@@ -1042,9 +1072,22 @@ EDITOR.prototype = {
             this.currentpage = this.pages.length - 1;
         }
         this.change_page();
+    },
+
+    /**
+     * Update any absolutely positioned nodes, within each drawable, when the drawing canvas is scrolled
+     */
+    move_canvas: function() {
+        var drawingregion, x, y, i;
+
+        drawingregion = Y.one(SELECTOR.DRAWINGREGION);
+        x = parseInt(drawingregion.get('scrollLeft'), 10);
+        y = parseInt(drawingregion.get('scrollTop'), 10);
+
+        for (i = 0; i < this.drawables.length; i++) {
+            this.drawables[i].scroll_update(x, y);
+        }
     }
-
-
 
 };
 
