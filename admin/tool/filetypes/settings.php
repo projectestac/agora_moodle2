f1ad1e8b1dc15d2f701a199d73e18c159e3877da
Sara Arjona <sarjona@xtec.cat>
2016-08-12 15:02:16 +0200

    Reviewed new parameters and hidden some to admin

--- admin/tool/filetypes/settings.php
+++ admin/tool/filetypes/settings.php
@@ -24,7 +24,14 @@
 
 defined('MOODLE_INTERNAL') || die;
 
+//XTEC ************ MODIFICAT - To let access only to xtecadmin user
+//2016.08.16 @sarjona
+if ($hassiteconfig & get_protected_agora()) {
+//************ ORIGINAL
+/*
 if ($hassiteconfig) {
+ */
+//************ FI
     $ADMIN->add('server', new admin_externalpage('tool_filetypes',
             new lang_string('pluginname', 'tool_filetypes'),
             $CFG->wwwroot . '/admin/tool/filetypes/index.php'));
