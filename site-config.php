3d9afc25bfda2d4fbfa7f605ff8e39d918368738
Toni Ginard <toni.ginard@gmail.com>
2020-04-08 11:21:04 +0200

    site-config: Removed code for old servers

--- site-config.php
+++ site-config.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once(INSTALL_BASE.'/html/config/dblib-mysql.php');
+require_once(INSTALL_BASE . '/html/config/dblib-mysql.php');
 
 global $school_info;
+
 $centre = getSchoolInfo('moodle2');
 
 if (isset($school_info['state_moodle2']) && ($school_info['state_moodle2'] == '-5')) {
@@ -23,24 +24,9 @@ if (isset($school_info['state_moodle2']) && ($school_info['state_moodle2'] == '-
     exit();
 }
 
-if ($agora['server']['enviroment'] == 'FRM') {
-    $currenthour = date('G');
-    // Change id for usu1 for training environment if is an odd hour (usu1 regenerates every odd hour)
-    if ($school_info['id_moodle2'] == 1 && $currenthour % 2 != 0) {
-        $school_info['id_moodle2'] = 10000;
-    }
-}
-
-if ($agora['moodle2']['dbtype'] == 'oci') {
-    $CFG->dbname = $school_info['database_moodle2'];
-    $CFG->dbuser = $agora['moodle2']['userprefix'] . $school_info['id_moodle2'];
-}
-
-if ($agora['moodle2']['dbtype'] == 'pgsql') {
-    $CFG->dbhost = $school_info['dbhost_moodle2'];
-    $CFG->dbname = $agora['moodle2']['userprefix'] . $school_info['id_moodle2'];
-    $CFG->dbuser = $agora['moodle2']['userprefix'] . $school_info['id_moodle2'];
-}
+$CFG->dbhost = $school_info['dbhost_moodle2'];
+$CFG->dbname = $agora['moodle2']['userprefix'] . $school_info['id_moodle2'];
+$CFG->dbuser = $agora['moodle2']['userprefix'] . $school_info['id_moodle2'];
 
 // Check for subdomain
 if (!empty($school_info['url_type']) && ($school_info['url_type'] == 'subdomain') && !empty($school_info['url_host'])) {
@@ -49,8 +35,9 @@ if (!empty($school_info['url_type']) && ($school_info['url_type'] == 'subdomain'
     $CFG->wwwroot = $agora['server']['html'] . $centre . '/moodle';
 }
 
-$CFG->dataroot  = INSTALL_BASE.'/'.get_filepath_moodle();
-if ( !empty($agora['server']['temp']) ) {
-    $CFG->tempdir = $agora['server']['temp'].'/'.get_filepath_moodle();
+$CFG->dataroot = INSTALL_BASE . '/' . get_filepath_moodle();
+if (!empty($agora['server']['temp'])) {
+    $CFG->tempdir = $agora['server']['temp'] . '/' . get_filepath_moodle();
 }
+
 $CFG->dnscentre = $centre;
