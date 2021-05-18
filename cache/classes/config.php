76b84db535e14f71e3c3552cea808b2afbc3500c
Toni Ginard <toni.ginard@gmail.com>
2021-05-18 18:34:38 +0200

    Ensure MUC is always configured

--- cache/classes/config.php
+++ cache/classes/config.php
@@ -113,6 +113,15 @@ class cache_config {
         global $CFG;
         if (!empty($CFG->altcacheconfigpath)) {
             $path = $CFG->altcacheconfigpath;
+
+            // XTEC ************ AFEGIT - Ensure MUC is configured
+            // @pferre22 2015.11.19
+            if (is_dir($path)) {
+                return $path . '/cacheconfig.php';
+            }
+            return $path;
+            // ************* FI
+
             if (is_dir($path) && is_writable($path)) {
                 // Its a writable directory, thats fine.
                 return $path.'/cacheconfig.php';
@@ -145,10 +154,24 @@ class cache_config {
         $this->configdefinitionmappings = array();
         $this->configlockmappings = array();
 
+        // XTEC ************ AFEGIT - Ensure MUC is configured
+        // siteidentifier must be located on settings.php and must be different for each instance
+        // 2014.08.12 @pferre22
+        if(isset($CFG->siteidentifier) && ! empty($CFG->siteidentifier)){
+            $siteidentifier = md5((string)$CFG->siteidentifier);
+        } else {
+        // ************ FI
+
         $siteidentifier = 'unknown';
         if (array_key_exists('siteidentifier', $configuration)) {
             $siteidentifier = $configuration['siteidentifier'];
         }
+
+        // XTEC ************ AFEGIT - Ensure MUC is configured
+        // 2014.08.12 @pferre22
+        }
+        // ************ FI
+
         $this->siteidentifier = $siteidentifier;
 
         // Filter the lock instances.
