4e18c8508c4573c1f264c9c8fd4b4aad1a7b3d7f
Toni Ginard <toni.ginard@gmail.com>
2023-01-12 13:42:30 +0100

    oAuth2 client: Only allow to login to users whose school code in IDI matches the school code of the Moodle site.

--- auth/oauth2/classes/auth.php
+++ auth/oauth2/classes/auth.php
@@ -316,7 +316,15 @@ class auth extends \auth_plugin_base {
                 continue;
             }
 
+            // XTEC ************ AFEGIT - Update data from oAuth2 to user profile fields. Default profile fields are updated
+            //                            out-of-the-box.
+            // 2023.01.17 @aginard
+            if (!property_exists($userdata, $fieldname) && !isset($userdata->profile[$fieldname])) {
+            /*
             if (!property_exists($userdata, $fieldname)) {
+            */
+            // ************ FI
+
                 // Just in case this field is on the list, but not part of the user data. This shouldn't happen though.
                 continue;
             }
@@ -333,7 +341,23 @@ class auth extends \auth_plugin_base {
             if ($lockvalue === 'unlocked' || ($lockvalue === 'unlockedifempty' && empty($oldvalue))) {
                 $value = (string)$value;
                 if ($oldvalue !== $value) {
+
+                    // XTEC ************ AFEGIT - Update data from oAuth2 to user profile fields. Default profile fields are updated
+                    //                            out-of-the-box.
+                    // 2023.01.17 @aginard
+                    if (isset($userdata->profile[$fieldname])) {
+                        $user->{'profile_field_' . $fieldname} = $value;
+                    } else {
+                    // ************ FI
+
                     $user->$fieldname = $value;
+
+                    // XTEC ************ AFEGIT - Update data from oAuth2 to user profile fields. Default profile fields are updated
+                    //                            out-of-the-box.
+                    // 2023.01.17 @aginard
+                    }
+                    // ************ FI
+
                 }
             }
         }
@@ -424,6 +448,52 @@ class auth extends \auth_plugin_base {
             $client->log_out();
             redirect(new moodle_url('/login/index.php'));
         }
+
+        // XTEC ************ AFEGIT - Login via IDI. Only users associated to the site code can log in.
+        // 2023.01.12 @aginard
+        if (is_agora()) {
+            $iditext = get_string('IDI', 'local_agora');
+            $isIDI = method_exists($client, 'get_issuer') && is_number(strpos($client->get_issuer()->get('loginpagename'), $iditext));
+            if ($isIDI) {
+                if (empty($userinfo['schoolcode'])) {
+                    $event = \core\event\user_login_failed::create([
+                        'other' => [
+                            'username' => $userinfo['username'],
+                            'reason' => get_string('codenotinidi', 'local_agora'),
+                        ],
+                    ]);
+                    $event->trigger();
+                    $SESSION->loginerrormsg = get_string('codenotinididetail', 'local_agora', [
+                        'username' => $userinfo['username'],
+                        'firstname' => $userinfo['firstname'],
+                        'lastname' => $userinfo['lastname'],
+                    ]);
+                    $client->log_out();
+                    redirect(new moodle_url('/login/index.php'));
+                } else {
+                    $canlogin = is_numeric(strpos($userinfo['schoolcode'], $CFG->center));
+                    if (!$canlogin) {
+                        $event = \core\event\user_login_failed::create([
+                            'other' => [
+                                'username' => $userinfo['username'],
+                                'reason' => get_string('codenotmatchidi', 'local_agora'),
+                            ],
+                        ]);
+                        $event->trigger();
+                        $SESSION->loginerrormsg = get_string('codenotmatchididetail', 'local_agora', [
+                            'username' => $userinfo['username'],
+                            'firstname' => $userinfo['firstname'],
+                            'code' => $userinfo['schoolcode'],
+                            'lastname' => $userinfo['lastname'],
+                            'center' => $CFG->center,
+                        ]);
+                        $client->log_out();
+                        redirect(new moodle_url('/login/index.php'));
+                    }
+                }
+            }
+        }
+        // ************ FI
 
         $userinfo['username'] = trim(core_text::strtolower($userinfo['username']));
         $oauthemail = $userinfo['email'];
@@ -589,6 +659,13 @@ class auth extends \auth_plugin_base {
                 } else {
                     // Create a new confirmed account.
                     $newuser = \auth_oauth2\api::create_new_confirmed_account($userinfo, $issuer);
+
+                    // XTEC ************ AFEGIT - Update data from oAuth2 to user profile fields. Default profile fields are updated
+                    //                            out-of-the-box.
+                    // 2023.01.17 @aginard
+                    $schoolcode = $userinfo['schoolcode'];
+                    // ************ FI
+
                     $userinfo = get_complete_user_data('id', $newuser->id);
                     // No redirect, we will complete this login.
                 }
@@ -599,6 +676,13 @@ class auth extends \auth_plugin_base {
         // method. Since we now ALWAYS link a login - if we get to here we can directly allow the user in.
         $user = (object) $userinfo;
         complete_user_login($user);
+
+        // XTEC ************ AFEGIT - Update data from oAuth2 to user profile fields. Default profile fields are updated
+        //                            out-of-the-box.
+        // 2023.01.17 @aginard
+        $this->update_user(['schoolcode' => $schoolcode], $user);
+        // ************ FI
+
         $this->update_picture($user);
         redirect($redirecturl);
     }
