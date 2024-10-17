6b994c1d2fcb91c376a4833e054433a269c9969e
Toni Ginard <toni.ginard@gmail.com>
2024-10-17 16:50:30 +0200

    Administration: Allow access only to xtecadmin (III)

--- report/backups/settings.php
+++ report/backups/settings.php
@@ -25,7 +25,17 @@
 
 defined('MOODLE_INTERNAL') || die;
 
+// XTEC ************ AFEGIT - Allow access only to xtecadmin.
+// 2024.10.16 @aginard
+if (get_protected_agora()) {
+// ************ FI
+
 $ADMIN->add('reports', new admin_externalpage('reportbackups', get_string('backups', 'admin'), "$CFG->wwwroot/report/backups/index.php",'moodle/backup:backupcourse'));
+
+// XTEC ************ AFEGIT - Allow access only to xtecadmin.
+// 2024.10.16 @aginard
+}
+// ************ FI
 
 // no report settings
 $settings = null;
