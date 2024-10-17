83816ef280ba60c990d55adf80844c34c64c3010
Toni Ginard <toni.ginard@gmail.com>
2024-10-17 16:50:09 +0200

    Administration: Allow access only to xtecadmin (II)

--- admin/tool/monitor/settings.php
+++ admin/tool/monitor/settings.php
@@ -25,6 +25,11 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
+// XTEC ************ AFEGIT - Allow access only to xtecadmin.
+// 2024.10.16 @aginard
+if (get_protected_agora()) {
+// ************ FI
+
 // Manage rules page.
 $temp = new admin_externalpage(
     'toolmonitorrules',
@@ -33,3 +38,8 @@ $temp = new admin_externalpage(
     'tool/monitor:managerules'
 );
 $ADMIN->add('reports', $temp);
+
+// XTEC ************ AFEGIT - Allow access only to xtecadmin.
+// 2024.10.16 @aginard
+}
+// ************ FI
