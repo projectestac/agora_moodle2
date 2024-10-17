8f0fe96dd916f6e13f0c9ee4254ddba3ca5cba0c
Toni Ginard <toni.ginard@gmail.com>
2024-10-17 16:50:09 +0200

    Administration: Allow access only to xtecadmin (II)

--- admin/tool/customlang/settings.php
+++ admin/tool/customlang/settings.php
@@ -23,4 +23,14 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+// XTEC ************ AFEGIT - Allow access only to xtecadmin.
+// 2024.10.16 @aginard
+if (get_protected_agora()) {
+// ************ FI
+
 $ADMIN->add('language', new admin_externalpage('toolcustomlang', get_string('pluginname', 'tool_customlang'), "$CFG->wwwroot/$CFG->admin/tool/customlang/index.php", 'tool/customlang:view'));
+
+// XTEC ************ AFEGIT - Allow access only to xtecadmin.
+// 2024.10.16 @aginard
+}
+// ************ FI
