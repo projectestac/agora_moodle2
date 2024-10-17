ef59eede0ab20ef6205f0366a40cf2dcc9641cd6
Toni Ginard <toni.ginard@gmail.com>
2024-10-17 16:50:54 +0200

    Administration: Allow access only to xtecadmin (IV)

--- admin/themeselector.php
+++ admin/themeselector.php
@@ -77,6 +77,13 @@ $index = 0;
 $currentthemeindex = 0;
 $themes = core_component::get_plugin_list('theme');
 
+// XTEC ************ AFEGIT - Allow access only to xtecadmin.
+// 2024.10.16 @aginard
+if (!get_protected_agora()) {
+    unset($themes['boost'], $themes['classic'], $themes['xtec2020']);
+}
+// ************ FI
+
 // Loop through available themes.
 foreach ($themes as $themename => $themedir) {
 
